3f3b25aa71ebad01a25b19e5f09e9f871f6807bf
Aniket Yadav <aniketyadav690@gmail.com>
2026-06-17 13:02:03 +0530

    update(server): cors update for production

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://rival-full-stack.vercel.app"],
     credentials: true,
   })
 );
